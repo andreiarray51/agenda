d51979553f1ceab4ef9e8bbc53c5645dda405cae
Andrei Avram <avramandrei@ymail.com>
2015-03-11 01:43:52 +0200

    Add token authentication to Calendar controller.

--- src/Array51/AgendaBundle/Controller/V1_1/CalendarController.php
+++ src/Array51/AgendaBundle/Controller/V1_1/CalendarController.php
@@ -3,13 +3,14 @@
 namespace Array51\AgendaBundle\Controller\V1_1;
 
 use FOS\RestBundle\Controller\FOSRestController as Controller;
+use Array51\AgendaBundle\Controller\TokenAuthenticatedControllerInterface;
 use Array51\AgendaBundle\Service\CalendarService;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Component\HttpFoundation\Request;
 use Array51\AgendaBundle\Response\V1_1\Calendar\CalendarResponse;
 
-class CalendarController extends Controller
+class CalendarController extends Controller implements TokenAuthenticatedControllerInterface
 {
     /**
      * @var CalendarService
@@ -38,6 +39,12 @@ class CalendarController extends Controller
      *          "required"="true",
      *          "default"="1.1",
      *          "description"="API Version"
+     *      },
+     *      {
+     *          "name"="token",
+     *          "dataType"="string",
+     *          "required"="true",
+     *          "description"="Access token"
      *      }
      *  },
      *  parameters={
@@ -50,6 +57,7 @@ class CalendarController extends Controller
      *  },
      * statusCodes={
      *      200="Returned when successful retrieved events list",
+     *      403="Returned when invalid access token",
      *      404="Returned when invalid parameters sent"
      *  }
      * )
