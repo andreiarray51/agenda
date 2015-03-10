34f472601e5cb81f51d91018e5c89438e25ddaf5
Andrei Avram <avramandrei@ymail.com>
2015-03-10 02:36:38 +0200

    Add query builder apply array filters.

--- src/Array51/DataBundle/Repository/AbstractEntityRepository.php
+++ src/Array51/DataBundle/Repository/AbstractEntityRepository.php
@@ -3,10 +3,29 @@
 namespace Array51\DataBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Query;
 
 class AbstractEntityRepository extends EntityRepository
 {
+    /**
+     * @param QueryBuilder $query
+     * @param array $filters
+     */
+    protected function applyFilters(QueryBuilder $query, array $filters)
+    {
+        $alias = $query->getRootAliases()[0];
+        array_walk(
+            $filters,
+            function ($value, $key) use ($query, $alias) {
+                if ($value) {
+                    $query->andWhere(sprintf('%s.%s = :%s', $alias, $key, $key))
+                        ->setParameter($key, $value);
+                }
+            }
+        );
+    }
+
     /**
      * @param Query $query
      * @return string
